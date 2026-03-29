2ec34f0cba5c6fd494daa5c019e88108eeed718f
Rodolfo Ulloa <rulloa@renta4.cl>
2026-03-29 19:54:01 -0300

    first commit

---
+++ src/main/java/com/rulloa/s3d/reservamedica/ReservamedicaApplication.java
@@ -0,0 +1,13 @@
+package com.rulloa.s3d.reservamedica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservamedicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservamedicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rulloa/s3d/reservamedica/ReservamedicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rulloa.s3d.reservamedica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservamedicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
